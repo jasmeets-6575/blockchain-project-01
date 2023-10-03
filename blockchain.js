97c2c393e71b823d6e9aeb0207f69ebf3da9b7f4
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-03 19:17:52 +0530

    adding validation

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,24 @@ class Blockchain {
     });
     this.chain.push(newBlock);
   }
+
+  static isvalidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, prevHash, hash, data } = chain[i];
+      const realLastHash = chain[i - 1].hash;
+      if (prevHash !== realLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, prevHash, data);
+      if (hash !== validatedHash) return false;
+    }
+    return true;
+  }
 }
 
+const blockchain = new Blockchain();
+blockchain.addBlock({ data: "block1" });
+const result = Blockchain.isvalidChain(blockchain.chain);
+console.log(result);
 module.exports = Blockchain;
